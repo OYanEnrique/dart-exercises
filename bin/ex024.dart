6c8ef38b9c1f288026fa2ee2c5601c3f7d39f2e1
OYanEnrique <enrique4work@gmail.com>
2025-10-10 00:13:35 -0300

    feat: adiciona exercício 24 - loop com break

---
+++ bin/ex024.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main(){
+  String frase ="Parou! Este código não continua.";
+  for(int i =0; i<frase.length; i++){
+    print(frase[i]);
+    if(frase[i] == "!"){
+      break;
+    }
+  }
+}
