61b852da167a0ed4a9ccb4388dc84e8305d2581e
OYanEnrique <enrique4work@gmail.com>
2025-10-10 00:13:11 -0300

    feat: adiciona exercício 21 - sistema de notas com menu

---
+++ bin/ex021.dart
@@ -0,0 +1,75 @@
+import 'dart:io';
+
+void main() {
+  List<String> notas = <String>[];
+  menu(notas);
+}
+
+String getComando(){
+  print("Digite um comando: 1 - Adicionar nota, 2 - Listar notas, 3 - Sair");
+  List<String> comandos = <String>["1", "2", "3"];
+  String? entrada = "";
+
+  entrada = stdin.readLineSync();
+
+
+  if(entrada == null || !comandos.contains(entrada)){
+    print("Comando inválido, tente novamente.");
+    getComando();
+  }
+
+  return entrada!;
+}
+
+List<String> adicionaNota(List<String> notas){
+  print("Escreva a nota a ser adicionada:");
+  String? nota = "";
+  nota = stdin.readLineSync();
+
+  if(nota != null && !nota.isEmpty){
+    notas.add(nota!);
+    print("Nota adicionada com sucesso!");
+  } else {
+    print("Nota inválida, tente novamente.");
+    adicionaNota(notas);
+  }
+
+  return notas;
+}
+
+void listarNotas(List<String> notas){
+  for(String nota in notas){
+    print(nota);
+  }
+}
+
+
+void menu(List<String> notas) {
+  print("");
+  cabecalho();
+  print("");
+  String comando = getComando();
+  print("");
+
+  switch (comando) {
+    case "1":
+      adicionaNota(notas);
+      menu(notas);
+    case "2":
+      listarNotas(notas);
+      menu(notas);
+    case "3":
+      print("Saindo...");
+    default:
+      print("Comando inválido, tente novamente.");
+      menu(notas);
+  }
+}
+
+void cabecalho(){
+  print("  _  _     _           ");
+  print(" | \\| |___| |_ __ _ ___");
+  print(" | .` / _ \\  _/ _` (_-<");
+  print(" |_|\\_\\___/\\__\\__,_/__/");
+  print("                       ");
+}
