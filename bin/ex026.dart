2b348a580e9cd6c4cd9e6eb5ddf49b9d64eedf57
OYanEnrique <enrique4work@gmail.com>
2025-10-11 15:04:01 -0300

    refactor: separa classe Conta para lib e adiciona encapsulamento no ex026

--- bin/ex026.dart
+++ bin/ex026.dart
@@ -1,4 +1,4 @@
-
+import '../lib/conta.dart';
 void main(){
   Conta contaYan = Conta("Yan", 1000.0);
   Conta contaEnrique = Conta("Enrique", 2000.0);
@@ -6,34 +6,18 @@ void main(){
   List<Conta> contas = <Conta>[contaYan, contaEnrique];
 
   for(Conta conta in contas){
-    print("Titular: ${conta.titular}, Saldo: \$${conta.saldo.toStringAsFixed(2)}");
+    conta.imprimeSaldo();
+    }
+
+
+  contaYan.enviar(200.0);
+  contaEnrique.receber(200.0);
+  print("Após transferência:");
+  
+  for(Conta conta in contas){
+    conta.imprimeSaldo();
   }
-  print(contaEnrique.saldo);
-  contaEnrique.saldo += 500.0;
-  print(contaEnrique.saldo);
-  receber(contaEnrique, 500.0);
-  print(contaEnrique.saldo);
-  enviar(contaEnrique, 1000.0);
-  print(contaEnrique.saldo);
-  enviar(contaEnrique, 5000.0);
-  print(contaEnrique.saldo);
 
 }
-void receber(Conta conta, double valor) {
-  conta.saldo += valor;
-}
 
-void enviar(Conta conta, double valor) {
-  if (conta.saldo >= valor) {
-    conta.saldo -= valor;
-  } else {
-    print("Saldo insuficiente para enviar \$${valor.toStringAsFixed(2)}");
-  }
-}
 
-class Conta{
-  String titular;
-  double saldo;
-
-  Conta(this.titular, this.saldo);
-}
