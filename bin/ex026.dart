911422e4b0810a80cff97755a8863bbd62ad45ff
OYanEnrique <enrique4work@gmail.com>
2025-10-13 00:30:17 -0300

    refactor: refatora ex026 para usar herança com diferentes tipos de conta

--- bin/ex026.dart
+++ bin/ex026.dart
@@ -2,16 +2,19 @@ import '../lib/conta.dart';
 void main(){
   Conta contaYan = Conta("Yan", 1000.0);
   Conta contaEnrique = Conta("Enrique", 2000.0);
+  ContaCorrente contaYenrique = ContaCorrente("Yenrique", 3000.0);
+  ContaPoupanca contaEnriquep = ContaPoupanca("Enriquep", 4000.0);
+  ContaSalario contaSalarioYan = ContaSalario("yanSalario", 5000.0, "00.000.000/0001-00", "Empresa X");
 
-  List<Conta> contas = <Conta>[contaYan, contaEnrique];
+  List<Conta> contas = <Conta>[contaYan, contaEnrique, contaYenrique, contaEnriquep];
 
   for(Conta conta in contas){
     conta.imprimeSaldo();
     }
 
+  contaSalarioYan.deposito(2000.0);
+  contaSalarioYan.imprimeSaldo();
 
-  contaYan.enviar(200.0);
-  contaEnrique.receber(200.0);
   print("Após transferência:");
   
   for(Conta conta in contas){
