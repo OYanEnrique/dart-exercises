135f3e2cf067c69efca510d38f3857053edc7639
OYanEnrique <enrique4work@gmail.com>
2025-10-13 00:31:18 -0300

    feat: adiciona exercício 31 - sistema de animais com polimorfismo

---
+++ bin/ex031.dart
@@ -0,0 +1,37 @@
+class Animal{
+  void emitirSom() {
+    print("O animal faz um som.");
+  }
+}
+class Cachorro extends Animal {
+  @override
+  void emitirSom() {
+    print("O cachorro late: Au Au!");
+  }
+
+  void abanarRabo() {
+    print("O cachorro está abanando o rabo.");
+  }
+
+}
+
+
+class Gato extends Animal {
+  @override
+  void emitirSom() {
+    print("O gato mia: Miau Miau!");
+  }
+
+  void arranharMoveis(){
+    print("O gato está arranhando os móveis.");
+  }
+}
+
+void main(){
+  Cachorro dog = Cachorro();
+  dog.emitirSom();
+  dog.abanarRabo();
+  Gato cat = Gato();
+  cat.emitirSom();
+  cat.arranharMoveis();
+}
