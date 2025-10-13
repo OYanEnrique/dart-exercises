039cd6cb513a8e69f4c2794b78af93739103b599
OYanEnrique <enrique4work@gmail.com>
2025-10-13 00:30:01 -0300

    feat: adiciona herança em lib/conta.dart com ContaCorrente, ContaPoupanca e ContaSalario

--- lib/conta.dart
+++ lib/conta.dart
@@ -24,3 +24,43 @@ void imprimeSaldo() {
 
 
 }
+
+class ContaCorrente extends Conta{
+double emprestimo = 300.0;
+
+  ContaCorrente(super.titular, super._saldo);
+
+  @override
+  void enviar(double valor) {
+    if (_saldo + emprestimo >= valor) {
+      _saldo -= valor;
+      imprimeSaldo();
+    } else {
+      print("Saldo insuficiente para enviar \$${valor.toStringAsFixed(2)}");
+    }
+  }
+}
+
+class ContaPoupanca extends Conta{
+  double rendimento = 0.05;
+
+  ContaPoupanca(super.titular, super._saldo);
+
+  void calcularRendimento(){
+    _saldo += _saldo * rendimento;
+    print("Rendimento aplicado. Novo saldo:");
+    imprimeSaldo();
+}
+}
+
+class ContaSalario extends Conta{
+  String cnpj;
+  String nomeEmpresa;
+  ContaSalario(super.titular, super._saldo, this.cnpj, this.nomeEmpresa);
+
+  void deposito(double valor){
+    _saldo += valor;
+    print("O salario da empresa $nomeEmpresa de CNPJ $cnpj no valor de \$${valor.toStringAsFixed(2)} foi depositado com sucesso.");
+  }
+
+}
