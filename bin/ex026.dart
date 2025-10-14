100b0aaa467b67e237849a0a9584018dab09b494
OYanEnrique <enrique4work@gmail.com>
2025-10-14 00:31:06 -0300

    refactor: simplifica ex026 removendo instâncias de Conta base

--- bin/ex026.dart
+++ bin/ex026.dart
@@ -1,12 +1,10 @@
 import '../lib/conta.dart';
 void main(){
-  Conta contaYan = Conta("Yan", 1000.0);
-  Conta contaEnrique = Conta("Enrique", 2000.0);
   ContaCorrente contaYenrique = ContaCorrente("Yenrique", 3000.0);
   ContaPoupanca contaEnriquep = ContaPoupanca("Enriquep", 4000.0);
   ContaSalario contaSalarioYan = ContaSalario("yanSalario", 5000.0, "00.000.000/0001-00", "Empresa X");
 
-  List<Conta> contas = <Conta>[contaYan, contaEnrique, contaYenrique, contaEnriquep];
+  List<Conta> contas = <Conta>[contaYenrique, contaEnriquep, contaSalarioYan];
 
   for(Conta conta in contas){
     conta.imprimeSaldo();
