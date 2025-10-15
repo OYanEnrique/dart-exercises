e7f189d2fce5142435d79da543ee8ed6dc6c0e7f
OYanEnrique <enrique4work@gmail.com>
2025-10-14 22:21:18 -0300

    feat: expande ex026 com ContaEmpresa e ContaInvestimento usando mixin

--- bin/ex026.dart
+++ bin/ex026.dart
@@ -3,8 +3,10 @@ void main(){
   ContaCorrente contaYenrique = ContaCorrente("Yenrique", 3000.0);
   ContaPoupanca contaEnriquep = ContaPoupanca("Enriquep", 4000.0);
   ContaSalario contaSalarioYan = ContaSalario("yanSalario", 5000.0, "00.000.000/0001-00", "Empresa X");
+  ContaEmpresa contaEmpresaX = ContaEmpresa("Empresa X", 10000.0);
+  ContaInvestimento contaInvestimentoYan = ContaInvestimento("Yan Investimento", 20000.0);
 
-  List<Conta> contas = <Conta>[contaYenrique, contaEnriquep, contaSalarioYan];
+  List<Conta> contas = <Conta>[contaYenrique, contaEnriquep, contaSalarioYan, contaEmpresaX, contaInvestimentoYan];
 
   for(Conta conta in contas){
     conta.imprimeSaldo();
@@ -12,6 +14,9 @@ void main(){
 
   contaSalarioYan.deposito(2000.0);
   contaSalarioYan.imprimeSaldo();
+  
+  contaEmpresaX.enviar(1000.0);
+  contaInvestimentoYan.receber(1000.0);
 
   print("Após transferência:");
   
